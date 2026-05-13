a9a411aa1f8debea19f7bc78a67d0f984e9abeda
ABEER <119006500+AbeerOsman@users.noreply.github.com>
2026-05-13 12:04:10 +0300

    Fix(iCloud): Comment the shema from BUDGIEApp

--- BUDGIE/BUDGIEApp.swift
+++ BUDGIE/BUDGIEApp.swift
@@ -4,37 +4,37 @@ import SwiftData
 @main
 struct BUDGIEApp: App {
     
-    var sharedModelContainer: ModelContainer = {
-        //Models that we have in the app
-        let schema = Schema([
-            User.self,
-            Category.self,
-            Transaction.self,
-            MerchantMapping.self,
-            DailyInsight.self,
-            WeeklyInsight.self,
-            MonthlyInsight.self
-        ] as! [any PersistentModel.Type])
-        
-        let modelConfiguration = ModelConfiguration(
-            schema: schema,
-            isStoredInMemoryOnly: false
-        )
-
-        do {
-            return try ModelContainer(
-                for: schema,
-                configurations: [modelConfiguration]
-            )
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+//    var sharedModelContainer: ModelContainer = {
+//        //Models that we have in the app
+//        let schema = Schema([
+//            User.self,
+//            Category.self,
+//            Transaction.self,
+//            MerchantMapping.self,
+//            DailyInsight.self,
+//            WeeklyInsight.self,
+//            MonthlyInsight.self
+//        ] as! [any PersistentModel.Type])
+//        
+//        let modelConfiguration = ModelConfiguration(
+//            schema: schema,
+//            isStoredInMemoryOnly: false
+//        )
+//
+//        do {
+//            return try ModelContainer(
+//                for: schema,
+//                configurations: [modelConfiguration]
+//            )
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Main()
         }
-        .modelContainer(sharedModelContainer)
+        //.modelContainer(sharedModelContainer)
     }
 }
---
+++ BUDGIE/View/Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  BUDGIE
+//
+//  Created by Abeer Jeilani Osman  on 26/11/1447 AH.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Main()
+}
